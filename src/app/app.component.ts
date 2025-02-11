70118c13f8cf5fd2f0bbc92b4200b043b8500936
Trey O'Neal <treyoneal90@gmail.com>
2025-02-10 18:46:03 -0600

    [ONEALTR] working products without booking or services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ImageWheelComponent } from './image-wheel/image-wheel.component';
 import { ServicesComponent } from './services/services.component';
-import { NavigationComponent } from "./navigation/navigation.component";
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ImageWheelComponent, ServicesComponent, NavigationComponent],
+  imports: [
+    RouterOutlet,
+    ImageWheelComponent,
+    ServicesComponent,
+    NavigationComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
